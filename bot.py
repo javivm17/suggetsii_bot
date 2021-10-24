2715b91c94c386a4e91f239f7e311ca6aee041db
Evelyn Gisele Yugsi Yugsi <eveyugyug@alum.us.es>
2021-10-24 14:49:13 +0200

    Updated welcome message
    Co-Authored-By: Javier Vilariño Mayo <44314228+javivm17@users.noreply.github.com>

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@ from telegram.ext import Updater, CommandHandler
 import os
 
 def start(update,context):
-    update.message.reply_text("Hola bienvenido al bot de sugerencias de la ETSII!")
+    update.message.reply_text("¡Hola soy SuggETSII! Aún estoy en fase de desarrollo. Os iré informando cuando tenga actualizaciones")
+
 
 if __name__ == "__main__":
     token = os.environ.get("TOKEN")
