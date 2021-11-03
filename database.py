f301c9bd2ecfd94e91084cb928a23b6d36646d46
javivm17 <javiervilarinyomayo.cr@gmail.com>
2021-11-03 10:03:39 +0100

    user and password coded from database

--- database.py
+++ database.py
@@ -1,14 +1,16 @@
 import pymysql
 import os
+import logging
 
 class Suggest_database:        
     def __init__(self):
         self.db = pymysql.connect(
-                host=os.environ.get("HOST"),
-                user=os.environ.get("USER"),
-                password=os.environ.get("PASSWORD"),
-                db= os.environ.get("DB")
+                host="sql11.freesqldatabase.com",
+                user="sql11447724",
+                password="JlaIeR9bT7",
+                db="sql11447724"
             )
+
         self.cursor = self.db.cursor()
 
     def insert_suggest(self,date,suggest):
@@ -44,4 +46,19 @@ class Suggest_database:
         except:
             # Rollback in case there is any error
             self.db.rollback()
-        self.db.close()  
+        self.db.close() 
+
+    def is_user_correct(self,user,password):
+        sql = "SELECT * FROM usuario"
+        try:
+            # Execute the SQL command
+            self.cursor.execute(sql)
+            result = self.cursor.fetchone()
+            if(result[1]==user and result[2]==password):
+                return True
+            else:
+                return False
+        except:
+            # Rollback in case there is any error
+            self.db.rollback()
+        self.db.close()   
